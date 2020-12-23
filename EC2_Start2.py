b71d51fe9331ed33a8586d510ac58a95defde7f9
MaorYahalomi <yahlomi@gmail.com>
2020-12-23 17:51:29 +0200

    commit

--- EC2_Start2.py
+++ EC2_Start2.py
@@ -1,7 +1,18 @@
 import boto3
-session = boto3.Session(aws_access_key_id="xx", aws_secret_access_key="xx")
+import json
 
-ec2 = session.resource('ec2',region_name='us-east-1',use_ssl=False)
-instance = ec2.Instance('i-0dcdb13da50d530b0')
-instance.start()
-print("Newssdfa-Maor")
+with open('..\\config.json') as f:
+   data = json.load(f)
+
+session = boto3.Session(
+    aws_access_key_id=data["aws_access_key_id"],
+    aws_secret_access_key=data["aws_secret_access_key"])
+
+ec2 = session.resource('ec2',region_name='us-east-2')
+instance1 = ec2.Instance('i-06280b0a82c1bc1c9')
+instance1.start()
+instance2 = ec2.Instance('i-0b937a7b63f78abf2')
+instance2.start()
+
+print("Starting Ec2 instances..")
+#    print(data["aws_access_key_id"])
